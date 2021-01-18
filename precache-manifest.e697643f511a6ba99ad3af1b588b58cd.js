b0870bb31673e2c6dc2871551fdf9658afbfc9bc
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-01-19 00:29:21 +0200

    Updates

---
+++ precache-manifest.e697643f511a6ba99ad3af1b588b58cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745dce8b2d7040247c4c7c5a4d2c0086",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9061423cf466cd3f1f06",
+    "url": "./static/css/main.07e3ae41.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9061423cf466cd3f1f06",
+    "url": "./static/js/main.012f7dfb.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
